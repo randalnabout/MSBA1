11736ea9fc698f8003769d43904c103c3c423d29
randalnabout <145806972+randalnabout@users.noreply.github.com>
2023-10-12 18:35:31 +0200

    Update 1.py

--- 1.py
+++ 1.py
@@ -11,61 +11,47 @@ data = pd.read_csv(DATA_URL)
 def main():
     st.title("World Happiness Report")
 
-    st.header("This page is part of my assignment for MSBA 325. First smile to continue.")
+    st.header("This page is part of my assignment MSBA 325. Data visualization.")
     st.write("Welcome to the World Happiness Report app. This app visualizes data from the 2019 World Happiness Report.")
     st.write("You can explore the happiness scores, GDP per capita, and more by placing the mouse on the country of interest.")
 
-    st.sidebar.title("Control Panel")
-    
-    st.sidebar.subheader("Your Happiness Percentage")
-    user_happiness = st.sidebar.slider("On a scale from 1 to 100, how happy are you?", min_value=1, max_value=100)
-    
-    # Calculate the user's relative rank based on their happiness percentage
-    user_rank = data[data["Score"] >= (user_happiness / 100) * data["Score"].max()].shape[0] + 1
-    
     st.subheader("Visualization 1: World Happiness Map")
     st.write("This map shows the happiness scores of different countries on the world map.")
-    
     fig5 = px.scatter_geo(data,
-                     locations="Country or region",
-                     locationmode="country names",
-                     color="Score",
-                     hover_name="Country or region",
-                     hover_data=["Score", "GDP per capita"],  # Add additional data for hovering
-                     size="GDP per capita",
-                     projection="natural earth",
-                     title="World Happiness Report")
+                         locations="Country or region",
+                         locationmode="country names",
+                         color="Score",
+                         hover_name="Country or region",
+                         hover_data=["Score", "GDP per capita"],  # Add additional data for hovering
+                         size="GDP per capita",
+                         projection="natural earth",
+                         title="World Happiness Report")
 
-fig5.update_geos(showcoastlines=True, coastlinecolor="Black", showland=True, landcolor="white", showocean=True, oceancolor="lightblue")
+    fig5.update_geos(showcoastlines=True, coastlinecolor="Black", showland=True, landcolor="white", showocean=True, oceancolor="lightblue")
 
-fig5.update_layout(geo=dict(showframe=False, showcoastlines=False))
+    fig5.update_layout(geo=dict(showframe=False, showcoastlines=False))
 
-st.plotly_chart(fig5)
+    st.plotly_chart(fig5)
 
+    fig6 = px.scatter(data_frame=data, x="GDP per capita", y="Score", animation_frame="Overall rank",
+                     size="Score", color="Country or region", hover_name="Country or region",
+                     title="Happiness Score vs. GDP per Capita (All Countries)")
+
+    fig6.update_layout(xaxis=dict(range=[1.2, 1.6]), yaxis=dict(range=[2, 8]))
+
+    st.plotly_chart(fig6)
 
     st.subheader("Find Country by Rank")
     st.write("Enter a rank to find the corresponding country:")
     rank = st.number_input("Enter a Rank", min_value=1, max_value=data["Overall rank"].max())
-
+    
     if st.button("Find Country"):
         selected_data = data[data["Overall rank"] == rank]
         if not selected_data.empty:
             country = selected_data.iloc[0]["Country or region"]
             st.write(f"Country at Rank {rank}: {country}")
         else:
-            st.write(f"No country found at Rank {rank}.")
-
-    st.sidebar.subheader("Find Similar Happiness Country")
-    st.write("See which country in the World Happiness Report is most similar to your happiness percentage.")
-    
-    # Calculate the absolute difference between user's happiness and each country's happiness
-    data["Difference"] = abs(data["Score"] - (user_happiness / 100))
-
-    # Find the country with the smallest difference
-    similar_country = data.loc[data["Difference"].idxmin()]["Country or region"]
-
-    st.sidebar.write(f"You are similar in happiness to the population of {similar_country} in the World Happiness Report.")
-    st.sidebar.write(f"Your rank relative to the World Happiness Report: {user_rank} out of 156 countries.")
+            st.write(f"No country found at Rank {rank}")
 
 if __name__ == "__main__":
     main()
