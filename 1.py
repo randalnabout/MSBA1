8356c40f81d106f440bf71d278c6fb64f7f17d06
randalnabout <145806972+randalnabout@users.noreply.github.com>
2023-10-12 18:31:21 +0200

    Update 1.py

--- 1.py
+++ 1.py
@@ -27,17 +27,21 @@ def main():
     st.write("This map shows the happiness scores of different countries on the world map.")
     
     fig5 = px.scatter_geo(data,
-                         locations="Country or region",
-                         locationmode="country names",
-                         color="Score",
-                         hover_name="Country or region",
-                         size="GDP per capita",
-                         projection="natural earth",
-                         title="World Happiness Report")
+                     locations="Country or region",
+                     locationmode="country names",
+                     color="Score",
+                     hover_name="Country or region",
+                     hover_data=["Score", "GDP per capita"],  # Add additional data for hovering
+                     size="GDP per capita",
+                     projection="natural earth",
+                     title="World Happiness Report")
 
-    fig5.update_geos(showcoastlines=True, coastlinecolor="Black", showland=True, landcolor="white", showocean=True, oceancolor="lightblue")
-    fig5.update_layout(geo=dict(showframe=False, showcoastlines=False))
-    st.plotly_chart(fig5)
+fig5.update_geos(showcoastlines=True, coastlinecolor="Black", showland=True, landcolor="white", showocean=True, oceancolor="lightblue")
+
+fig5.update_layout(geo=dict(showframe=False, showcoastlines=False))
+
+st.plotly_chart(fig5)
+
 
     st.subheader("Find Country by Rank")
     st.write("Enter a rank to find the corresponding country:")
