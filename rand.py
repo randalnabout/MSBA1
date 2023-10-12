ee2805b6d6c4e1aaedf9f564f6f98bd20af77707
randalnabout <145806972+randalnabout@users.noreply.github.com>
2023-10-12 15:38:14 +0200

    Update rand.py

--- rand.py
+++ rand.py
@@ -16,47 +16,46 @@ def main():
     st.write("You can explore the happiness scores, GDP per capita, and more by placing the mouse on the country of interest.")
 
     # Modify Figure 5
-st.subheader("Visualization 1: World Happiness Map")
-st.write("This map shows the happiness scores of different countries on the world map. Hover over countries to see details.")
+    st.subheader("Visualization 1: World Happiness Map")
+    st.write("This map shows the happiness scores of different countries on the world map. Hover over countries to see details.")
 
-fig5 = px.scatter_geo(filtered_data,
-                     locations="Country or region",
-                     locationmode="country names",
-                     color="Score",
-                     hover_name="Country or region",
-                     size="GDP per capita",
-                     projection="natural earth",
-                     title="World Happiness Report",
-                     hover_data={"Country or region": True, "Score": ":.2f", "GDP per capita": ":.2f"})
+    fig5 = px.scatter_geo(filtered_data,
+                         locations="Country or region",
+                         locationmode="country names",
+                         color="Score",
+                         hover_name="Country or region",
+                         size="GDP per capita",
+                         projection="natural earth",
+                         title="World Happiness Report",
+                         hover_data={"Country or region": True, "Score": ":.2f", "GDP per capita": ":.2f"})
 
-fig5.update_geos(showcoastlines=True, coastlinecolor="Black", showland=True, landcolor="white", showocean=True, oceancolor="lightblue")
+    fig5.update_geos(showcoastlines=True, coastlinecolor="Black", showland=True, landcolor="white", showocean=True, oceancolor="lightblue")
 
-fig5.update_layout(geo=dict(showframe=False, showcoastlines=False))
+    fig5.update_layout(geo=dict(showframe=False, showcoastlines=False))
 
-st.plotly_chart(fig5)
+    st.plotly_chart(fig5)
 
-# ...
+    # ...
 
-fig6 = px.scatter(data_frame=filtered_data, x="GDP per capita", y="Score", animation_frame="Overall rank",
-                 size="Score", color="Country or region", hover_name="Country or region",
-                 title="Happiness Score vs. GDP per Capita (All Countries)")
+    fig6 = px.scatter(data_frame=filtered_data, x="GDP per capita", y="Score", animation_frame="Overall rank",
+                     size="Score", color="Country or region", hover_name="Country or region",
+                     title="Happiness Score vs. GDP per Capita (All Countries)")
 
-fig6.update_layout(xaxis=dict(range=[1.2, 1.6]), yaxis=dict(range=[2, 8]))
+    fig6.update_layout(xaxis=dict(range=[1.2, 1.6]), yaxis=dict(range=[2, 8]))
 
-st.plotly_chart(fig6)
+    st.plotly_chart(fig6)
 
     st.subheader("Find Country by Rank")
     st.write("Enter a rank to find the corresponding country:")
     rank = st.number_input("Enter a Rank", min_value=1, max_value=data["Overall rank"].max())
-    
+
     if st.button("Find Country"):
         selected_data = data[data["Overall rank"] == rank]
         if not selected_data.empty:
             country = selected_data.iloc[0]["Country or region"]
             st.write(f"Country at Rank {rank}: {country}")
         else:
-            st.write(f"No country found at Rank {rank}.")
+            st.write(f"No country found at Rank {rank}")
 
 if __name__ == "__main__":
     main()
-
